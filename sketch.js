648ccb6ef4393822d3ddc30210f030027805ce73
Phil Turner <julietius@gmail.com>
2017-06-10 15:19:36 +0100

    Added movement death mechanic

--- sketch.js
+++ sketch.js
@@ -1,15 +1,21 @@
 var GRID_SIZE = 64;
 var COLS = 8;
 var ROWS = 9;
-var MAX_CARS = 10;
+var MAX_CARS = 6;
 var MARGIN = 32;
 
-var player
-var cars
-var logs
+var player;
+var imgdeath;
+var cars;
+var logs;
+var dead = false;
+
+function preload(){
+  imgdeath = loadImage('assets/frog-death.png');
+}
 
 function setup(){
-  var canvas = createCanvas(COLS * GRID_SIZE , ROWS * GRID_SIZE);
+  var canvas = createCanvas(ROWS * GRID_SIZE , COLS * GRID_SIZE);
   canvas.parent('canvas-container');
 
   cars = new Group();
@@ -21,16 +27,16 @@ function setup(){
     var row = floor(random(2, 5)) * GRID_SIZE;
     var py = height - row;
     var px = floor(random(0, ROWS)) * GRID_SIZE;
-    createCar(1, px, py, ang);  //type
+    createCar(1, px, py + (GRID_SIZE/2), ang);  //type
   }
 
   //setup player
   player = createSprite(width/2, height/2);
   var img  = loadImage("assets/frog.png");
   player.addImage(img);
-  player.scale = .9;
+  player.scale = 1;
   player.position.x = floor(ROWS / 2) * GRID_SIZE;
-  player.position.y = height - GRID_SIZE;
+  player.position.y = height - (GRID_SIZE/2);
 }
 
 function draw(){
@@ -47,19 +53,26 @@ function draw(){
 
   //update sprites & draw
   drawSprites();
+  player.collide(cars, collided);
+}
+
+function collided() {
+  dead = true;
+  player.removeImage;
+  player.addImage(imgdeath);
 }
 
 function createCar(type, x, y, a){
   var c = createSprite(x, y);
   var img  = loadImage("assets/car.png");  //TODO Use type below to add different images
   c.addImage(img);
-  c.setSpeed(4.5, a);
+  c.setSpeed(3, a);
   c.rotationSpeed = 0;
   c.type = type;
 
   //TODO Build different car images and sizes? extend to lorry or something ?!?
   if(type == 1)
-    c.scale = 0.98;
+    c.scale = 1;
   if(type == 2)
     c.scale = .6;
   if(type == 3)
@@ -67,8 +80,22 @@ function createCar(type, x, y, a){
 
   c.mass =  2 + c.scale;
 
-  //c.setCollider("rectangle", 250, 250, 200, 200); //TODO Potentially use for collision
+  //c.setCollider("rectangle", 0, 0, 0, 0); //TODO Potentially use for collision
   cars.add(c);
   return c;
+}
 
+function keyPressed() {
+  if (!dead){ //stops movement if dead
+    if(keyCode == UP_ARROW) {
+      player.position.y += -GRID_SIZE
+    } else if (keyCode == DOWN_ARROW) {
+      player.position.y += GRID_SIZE
+    } else if (keyCode == LEFT_ARROW) {
+      player.position.x += -GRID_SIZE
+    } else if (keyCode == RIGHT_ARROW) {
+      player.position.x += GRID_SIZE
+    }
+  }
+  return 0;
 }
