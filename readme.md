8320c6266b48815816fdccbd22105e6b808f2543
Phil Turner <julietius@gmail.com>
2017-06-10 13:17:59 +0100

    Initial setup commit

---
+++ readme.md
@@ -0,0 +1,2 @@
+<h1>Frogger Game</h1>
+<p>Built using p5js-play - enjoy</p>
---
+++ sketch.js
@@ -0,0 +1,13 @@
+
+var GRID_SIZE = 64;
+var COLS = 8;
+var ROWS = 7;
+
+function setup(){
+  var canvas = createCanvas(COLS * GRID_SIZE ,ROWS * GRID_SIZE);
+  canvas.parent('canvas-container');
+}
+
+function draw(){
+  background(61);
+}
